4403ed348b93fa0f2936aab584880ac7be0c609c
Arthur Fellype <arthurfellypepe@gmail.com>
2026-08-23 19:49:22 -0300

    adicionarAluno - FAIL

---
+++ testes/Turma.test.js
@@ -0,0 +1,13 @@
+import Aluno from "../models/Aluno.js";
+import Turma from "../models/Turma.js";
+
+describe("Turma", () => {
+	test("adicionarAluno adiciona um aluno à turma", () => {
+		const turma = new Turma("3A");
+		const aluno = new Aluno("João", [8, 8, 8], "3A");
+
+		turma.adicionarAluno(aluno);
+
+		expect(turma.alunos).toHaveLength(2);
+	});
+}); 
